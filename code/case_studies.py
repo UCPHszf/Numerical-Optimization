9f093bf92ab7caf07c252a674cd8f3a9e1a54e33
Yufei Yuan <yuanyufei1999@gmail.com>
2022-02-09 21:49:27 +0100

    Replace h with better representation

--- code/case_studies.py
+++ code/case_studies.py
@@ -17,13 +17,11 @@ def log_ellipsoid(x: List[float], epsilon: float = 1e-4) -> float:
 
 def attractive_sector(x: List[float], q: float = 1e4) -> float:
     d = len(x)
-    h = lambda x : np.log(1 + np.exp(q * x)) / q
+    # h = lambda x : np.log(1 + np.exp(q * x)) / q
+    h = lambda x : (np.log(1 + np.exp(-abs(q * x))) + max(q * x, 0)) / q
     return sum([np.exp2(h(x[i])) + 100 * np.exp2(h(x[i])) for i in range(d)])
 
 
 def sum_of_different_powers_function(x: List[float]) -> float:
     d = len(x)
     return sum([np.power(np.exp2(x[i]), 1 + (i - 1) / (d - 1)) for i in range(d)])
-
-if __name__ == '__main__':
-    print("Hello, World!")
